589b5f1d142d20550f22d3fd73ebe602cd057e23
Tnze <cjd001113@outlook.com>
2019-11-02 13:25:14 +0800

    电机驱动改用class实现

--- motor.cpp
+++ motor.cpp
@@ -1,34 +1,32 @@
 #include <Arduino.h>
 #include "motor.h"
 
-Motor motor_1 = {PIN_MOTOR_1, 0},
-      motor_2 = {PIN_MOTOR_2, 0};
-
-void motor_setup()
+void init_motor()
 {
     //  初始化管脚
     pinMode(motor_1.stepPin, OUTPUT);
     pinMode(motor_2.stepPin, OUTPUT);
     pinMode(PIN_TRAVEL_SWITCH_1, INPUT_PULLUP);
     pinMode(PIN_TRAVEL_SWITCH_2, INPUT_PULLUP);
+
+    motor_1 = {PIN_MOTOR_1, 0};
+    motor_2 = {PIN_MOTOR_2, 0};
 }
 
-//步进电机向前移动1步
-void Motor_Step(Motor *m)
+void Motor::Step()
 {
-    digitalWrite(m->stepPin, HIGH);
+    digitalWrite(stepPin, HIGH);
     delayMicroseconds(3);
-    digitalWrite(m->stepPin, LOW);
+    digitalWrite(stepPin, LOW);
     delayMicroseconds(2);
 }
 
-//步进电机在time(ms)指定的时间移动指定step步
-void Motor_Move(Motor *m, int moveStep, int moveTime)
+void Motor::Move(int step, int time)
 {
-    int delayTime = (moveTime * 1000 - moveStep * 5) / moveStep;
-    for (int i = 0; i < moveStep; i++)
+    int delayTime = (time * 1000 - step * 5) / step;
+    for (int i = 0; i < step; i++)
     {
-        Motor_Step(m);
+        Step();
         delayMicroseconds(delayTime);
     }
 }
--- motor.h
+++ motor.h
@@ -1,9 +1,17 @@
 #include <Arduino.h>
 #include "writer_config.h"
 
-typedef struct
+void init_motor(); // 初始化电机
+
+class Motor
 {
-    int stepPin;  //pin for Motor step port
-    int dirPin;   //pin for Motor direction port
-    int location; //Motor Now location
-} Motor;
+public:
+    int stepPin;  // pin for Motor step port
+    int dirPin;   // pin for Motor direction port
+    int location; // Motor Now location
+
+    void Step();                   // 步进电机向前移动1步
+    void Move(int step, int time); // 步进电机在time(ms)指定的时间移动指定step步
+};
+
+Motor motor_1, motor_2;
