b1e313a00e8d587765cc7d63bf381ba7fa9eb036
Tnze <cjd001113@outlook.com>
2019-11-26 23:18:44 +0800

    串口通信校验

--- upper/run.go
+++ upper/run.go
@@ -1,49 +1,110 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"log"
-	"math"
 	"os"
 	"strings"
-	"time"
 
+	"github.com/mattn/go-colorable"
 	"github.com/tarm/serial"
 )
 
+const (
+	norm   = "\033[0m"
+	yellow = "\033[33m"
+	green  = "\033[32m"
+	blue   = "\033[34m"
+	red    = "\033[31m"
+)
+
+var sendLogger = log.New(colorable.NewColorableStdout(), yellow+"--->", log.LstdFlags)
+var recvLogger = log.New(colorable.NewColorableStdout(), blue+"<---", log.LstdFlags)
+var erroLogger = log.New(colorable.NewColorableStdout(), red+"error", log.LstdFlags)
+var normLogger = log.New(colorable.NewColorableStdout(), norm, log.LstdFlags)
+var succLogger = log.New(colorable.NewColorableStdout(), green+"success", log.LstdFlags)
+
+var okWaiter = make(chan string, 10) // 命令确认通道，容量决定了发送到下位机的最多命令数
+
 func main() {
+	log.SetOutput(colorable.NewColorableStdout())
+
 	c := &serial.Config{Name: "/dev/cu.SLAB_USBtoUART", Baud: 115200}
 	s, err := serial.OpenPort(c)
 	if err != nil {
-		log.Fatal(err)
+		normLogger.Fatal(err)
 	}
 	go func() {
+		// 发送文件
 		if len(os.Args) > 1 {
-			gcode, err := ioutil.ReadFile(os.Args[1])
+			f, err := os.Open(os.Args[1])
 			if err != nil {
-				log.Fatal(err)
+				sendLogger.Fatal("open file error: ", err)
 			}
-			for _, v := range strings.Split(string(gcode), "\n") {
-				var (
-					g    int
-					x, y float64
-				)
-				fmt.Sscanf(v, "G%d X%f Y%f", &g, &x, &y)
-				sleeptime := time.Millisecond*20 + time.Duration(float64(time.Millisecond)*20*math.Sqrt(x*x+y*y))
-				fmt.Printf("---%v-->%q (%f, %f)\n", sleeptime, v, x, y)
-				time.Sleep(sleeptime)
-				_, err = s.Write([]byte(v + "\n"))
-				if err != nil {
-					log.Fatal(err)
+			scanner := bufio.NewScanner(f)
+			for scanner.Scan() {
+				gcode := scanner.Bytes()
+				if len(gcode) > 0 && gcode[0] == 'G' {
+					okWaiter <- string(gcode)
 				}
-
+				if err := sendLine(s, gcode); err != nil {
+					sendLogger.Fatal(err)
+				}
 			}
 		}
 
-		io.Copy(s, os.Stdin)
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			line := scanner.Bytes()
+
+			if len(line) > 0 && line[0] == 'G' {
+				okWaiter <- string(line)
+			}
+			if err := sendLine(s, line); err != nil {
+				sendLogger.Fatal(err)
+			}
+		}
+		if err := scanner.Err(); err != nil {
+			sendLogger.Fatal(err)
+		}
 	}()
 
-	io.Copy(os.Stdout, s) // 打印串口接收到到数据
+	// 打印串口接收到到数据
+	r := bufio.NewReader(s)
+	for {
+		line, _, err := r.ReadLine()
+		if err != nil {
+			recvLogger.Fatal(err)
+		}
+		strline := string(line)
+
+		if strings.HasPrefix(strline, "//") {
+			strline = norm + strline
+		} else if strings.HasSuffix(strline, "ok") {
+			<-okWaiter
+			strline = green + strline
+		} else if strings.HasSuffix(strline, "rs") {
+			strline = red + strline
+		}
+
+		recvLogger.Print(strline)
+	}
+}
+
+var lastLineCode int
+
+func sendLine(s io.Writer, gcode []byte) error {
+	prefix := fmt.Sprintf("N%d %s", lastLineCode+1, gcode)
+	lastLineCode++
+	// 计算校验码
+	var cs byte
+	for i := range prefix {
+		cs = cs ^ prefix[i]
+	}
+	pl := fmt.Sprintf("%s*%d\n", prefix, cs)
+	sendLogger.Print(pl)
+	_, err := s.Write([]byte(pl))
+	return err
 }
--- writer.ino
+++ writer.ino
@@ -18,46 +18,104 @@ void setup()
     Serial.begin(115200);
     init_servo();
     move_servo(PEN_UP);
-    Serial.printf("printer lunched");
+    Serial.println("//printer lunched");
 }
 
-int pen_status = 0; // 0为提笔，1为落笔
+int pen_status = 0; // 笔的状态，0为提笔，1为落笔
+
+// 执行G-code，注意该解释器是随手写的，不健壮
 int exec_gcode(String line)
 {
-    if (line.length())
-        Serial.println(line);
-
+    Serial.println("// received: " + line);
+    String args;
+    int first_space, num;
     switch (line[0])
     {
     case 'G':
-        int first_space = line.indexOf(' ');
-        int num = line.substring(1, first_space).toInt();
+        first_space = line.indexOf(' ');
+        num = line.substring(1, first_space).toInt();
         if (pen_status != num) // 需要调整笔到高度
         {
-            Serial.println("pen " + num ? "down" : "up");
+            // Serial.println("pen " + num ? "down" : "up");
             move_servo((pen_status = num) ? PEN_DOWN : PEN_UP);
             delay(300); // 等待舵机就位
         }
 
-        String args = line.substring(first_space + 1);
+        args = line.substring(first_space + 1);
         float x, y;
-        for (int i = 0; i < 2; i++) // 读取目标位置
+
+        for (;;) // 读取目标位置
         {
             char c = args[0];
             int fs = args.indexOf(' ');
-            (c == 'X' ? x : y) = args.substring(1, fs).toFloat();
+            (c == 'X' ? x : y) = (fs == -1 ? args.substring(1) : args.substring(1, fs)).toFloat();
+            if (fs == -1)
+                break;
             args = args.substring(fs + 1);
         }
-        Serial.printf("Move to (%f, %f)\n", x, y);
+        Serial.printf("// move to (%f, %f)\n", x, y);
         motors.MoveTo(x, y);
         break;
+
+    default:
+        Serial.println("// unknown command " + line);
     }
+
+    Serial.println("ok");
     return 0;
 }
 
+// 检查N代码是否正确，并去除它
+bool checkCMD(String &cmd, int &line_code)
+{
+    // 如果没有校验则直接返回true
+    if (cmd[0] != 'N')
+        return true;
+
+    int start = cmd.indexOf(' ');
+    int end = cmd.lastIndexOf('*');
+
+    if (start == -1 || end == -1)
+        return false; // 格式不正确
+
+    // 计算码字
+    line_code = cmd.substring(1, start).toInt();    // 行码
+    int chek_code = cmd.substring(end + 1).toInt(); // 校验码
+
+    // 计算本地校验码
+    int cs = 0;
+    for (int i = 0; i != end; i++)
+        cs = cs ^ cmd[i];
+    cs &= 0xff; // Defensive programming...
+
+    // 去除原字符串校验部分
+    cmd = cmd.substring(start + 1, end);
+
+    return cs == chek_code;
+}
+
+int last_line_code = 0;
 void loop()
 {
     // 从串口接受指令
-    String cmd = Serial.readStringUntil('\n');
-    exec_gcode(cmd);
+    String cmd = Serial.readStringUntil('\n'); // 读取一行
+
+    if (!cmd.length())
+        return;
+
+    cmd = cmd.substring(0, cmd.indexOf(';')); // 去除注释
+
+    int line_code;
+
+    if (!checkCMD(cmd, line_code)) // 检查不通过，要求重发
+        Serial.printf("rs %d error: check code error\n", line_code);
+    else if (cmd[0] == 'N' && line_code != last_line_code + 1) // 行号不对，要求重发
+        Serial.printf("rs %d error: line number is not current line + 1. last line: %d\n",
+                      last_line_code + 1,
+                      last_line_code);
+    else
+    {
+        exec_gcode(cmd);
+        last_line_code++;
+    }
 }
